aa55227f796614277e107787f5cdcf97af807d16
RakeshRaj B S <rajr095025@gmail.com>
2022-08-24 13:06:15 +0530

    Final Project RockPaperScissor

--- script/script.js
+++ script/script.js
@@ -1,7 +1,3 @@
-function getComputerChoice() {
-    let values = ["rock","paper","scissor"]
-    return values[Math.floor(Math.random() * values.length)]
-}
 const rockClicked = document.querySelector('#rock');
 rockClicked.addEventListener('click', () => game("rock"));
 
@@ -15,60 +11,68 @@ let playerWonCount = 0;
 let computerWonCount = 0;
 let matchDrawnCount = 0;
 
-function playRound(playerSelection){
-    let computerSelection = getComputerChoice();
-    if((playerSelection == "rock" && computerSelection == "scissor")||
-    (playerSelection == "scissor" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")){
-        return "playerWon";
-    } else if((playerSelection == "rock" && computerSelection == "rock")||
-    (playerSelection == "paper" && computerSelection == "paper") ||
-    (playerSelection == "scissor" && computerSelection == "scissor")){
-        return "matchDrawn";
-
-    } else if((playerSelection == "rock" && computerSelection == "paper")||
-    (playerSelection == "scissor" && computerSelection == "rock") ||
-    (playerSelection == "paper" && computerSelection == "scissor")){
-        return "computerWon";
-    }
-}
-
 function game(playerSelection){
     let finalResult = playRound(playerSelection);
     console.log(finalResult);
-    document.getElementById("demo1").innerHTML = finalResult;
+    document.getElementById("roundresult").innerHTML = finalResult;
     
-    if (finalResult == "playerWon"){
+    if (finalResult == "Player Won"){
         playerWonCount++
-    } else if (finalResult == "computerWon"){
+    } else if (finalResult == "Computer Won"){
         computerWonCount++
-    } else if (finalResult == "matchDrawn"){
+    } else if (finalResult == "Match Drawn"){
         matchDrawnCount++
     }
+
+    document.getElementById("playerwoncount").innerHTML = playerWonCount;
+    document.getElementById("matchdrawncount").innerHTML = matchDrawnCount;
+    document.getElementById("computerwoncount").innerHTML = computerWonCount;
 
     console.log(playerWonCount);
     console.log(computerWonCount);
     console.log(matchDrawnCount);
 
-    if (playerWonCount == 4 ){
-        document.getElementById("demo2").innerHTML = "Player won 5 times first";
-    } else if (computerWonCount == 4 ){
-        document.getElementById("demo2").innerHTML = "Computer won 5 times first";
+    document.getElementById("finalresult").innerHTML = "";
+
+    if (playerWonCount == 5 ){
+        document.getElementById("finalresult").innerHTML = "Player won first 5 times";
+        playerWonCount = 0
+        computerWonCount = 0
+        matchDrawnCount = 0
+    } else if (computerWonCount == 5 ){
+        document.getElementById("finalresult").innerHTML = "Computer won first 5 times";
+        playerWonCount = 0
+        computerWonCount = 0
+        matchDrawnCount = 0
     }
-
-
-
 
 }
 
+function getComputerChoice() {
+    let values = ["rock","paper","scissor"]
+    return values[Math.floor(Math.random() * values.length)]
+}
 
-//let finalResult = playRound();
-//document.getElementById("demo").innerHTML = playRound();
-//console.log(finalResult);
-//let result = document.querySelector('#results')
-//let round = document.createElement('div')
-//round.textContent = playRound();
-//result.appendChild(round)
+
+function playRound(playerSelection){
+    let computerSelection = getComputerChoice();
+    if((playerSelection == "rock" && computerSelection == "scissor")||
+    (playerSelection == "scissor" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")){
+        return "Player Won";
+    } else if((playerSelection == "rock" && computerSelection == "rock")||
+    (playerSelection == "paper" && computerSelection == "paper") ||
+    (playerSelection == "scissor" && computerSelection == "scissor")){
+        return "Match Drawn";
+
+    } else if((playerSelection == "rock" && computerSelection == "paper")||
+    (playerSelection == "scissor" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissor")){
+        return "Computer Won";
+    }
+}
+
+
 
   
 
